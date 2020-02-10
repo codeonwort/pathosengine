92c4ffdf380406b49650332a054eabb07f582f2f
codeonwort <codeonwort@gmail.com>
2020-02-10 13:10:58 +0900

    Engine version 0.4.0 -> 0.5.0

--- projects/PathosEngine/src/pathos/engine_version.h
+++ projects/PathosEngine/src/pathos/engine_version.h
@@ -2,5 +2,5 @@
 
 // I doubt ever this will be managed properly... but let's define anyway.
 #define PATHOS_MAJOR_VERSION 0
-#define PATHOS_MINOR_VERSION 4
+#define PATHOS_MINOR_VERSION 5
 #define PATHOS_PATCH_VERSION 0
