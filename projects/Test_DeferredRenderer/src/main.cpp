b1aaaa35a9f854e723eb43cbf79892a23f0af0e3
codeonwort <codeonwort@gmail.com>
2020-06-01 05:37:32 +0900

    Move up/down by keyboard E/Q

--- projects/Test_DeferredRenderer/src/main.cpp
+++ projects/Test_DeferredRenderer/src/main.cpp
@@ -112,6 +112,10 @@ void setupInput()
 	moveRight.addInput(InputConstants::KEYBOARD_D, 1.0f);
 	moveRight.addInput(InputConstants::KEYBOARD_A, -1.0f);
 
+	AxisBinding moveUp;
+	moveUp.addInput(InputConstants::KEYBOARD_E, 1.0f);
+	moveUp.addInput(InputConstants::KEYBOARD_Q, -1.0f);
+
 	AxisBinding moveFast;
 	moveFast.addInput(InputConstants::SHIFT, 1.0f);
 
@@ -127,6 +131,7 @@ void setupInput()
 	InputManager* inputManager = gEngine->getInputSystem()->getDefaultInputManager();
 	inputManager->bindAxis("moveForward", moveForward);
 	inputManager->bindAxis("moveRight", moveRight);
+	inputManager->bindAxis("moveUp", moveUp);
 	inputManager->bindAxis("moveFast", moveFast);
 	inputManager->bindButtonPressed("drawShadowFrustum", drawShadowFrustum, setupCSMDebugger);
 	inputManager->bindButtonPressed("updateSceneCapture", updateSceneCapture, []()
@@ -404,18 +409,21 @@ void tick(float deltaSeconds)
 
 		// movement per seconds
 		const float moveMultiplier = pathos::max(1.0f, input->getAxis("moveFast") * 10.0f);
-		const float speedRight = 400.0f * deltaSeconds * moveMultiplier;
-		const float speedForward = 200.0f * deltaSeconds * moveMultiplier;
-		const float rotateYaw = 120.0f * deltaSeconds;
-		const float rotatePitch = 120.0f * deltaSeconds;
+		const float speedRight     = 200.0f * deltaSeconds * moveMultiplier;
+		const float speedForward   = 200.0f * deltaSeconds * moveMultiplier;
+		const float speedUp        = 200.0f * deltaSeconds * moveMultiplier;
+		const float rotateYaw      = 120.0f * deltaSeconds;
+		const float rotatePitch    = 120.0f * deltaSeconds;
 
 		float deltaRight   = input->getAxis("moveRight") * speedRight;
 		float deltaForward = input->getAxis("moveForward") * speedForward;
+		float deltaUp      = input->getAxis("moveUp") * speedUp;
 		float rotY         = 0.1f * (currMouseX - prevMouseX) * rotateYaw;
 		float rotX         = 0.1f * (currMouseY - prevMouseY) * rotatePitch;
 
 		cam->moveForward(deltaForward);
 		cam->moveRight(deltaRight);
+		cam->moveUp(deltaUp);
 		if (rotateByMouse) {
 			cam->rotateYaw(rotY);
 			cam->rotatePitch(rotX);
