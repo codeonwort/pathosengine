d4f864be90f591e811ba5e1a6f4d67d775f8b552
codeonwort <codeonwort@gmail.com>
2021-06-13 17:36:12 +0900

    wip: precomputed atmospheric scattering (transmittance LUT)

--- projects/PathosEngine/src/pathos/render/sky_atmosphere.cpp
+++ projects/PathosEngine/src/pathos/render/sky_atmosphere.cpp
@@ -1,6 +1,69 @@
 #include "sky_atmosphere.h"
 #include "render_device.h"
 #include "pathos/shader/shader.h"
+#include "pathos/shader/shader_program.h"
+
+// #todo-atmosphere: for test
+#include "pathos/mesh/geometry_primitive.h"
+#include "pathos/util/engine_util.h"
+#include "pathos/util/log.h"
+
+// Precomputed Atmospheric Scattering
+namespace pathos {
+
+	class PASFullscreenVS : public ShaderStage {
+	public:
+		PASFullscreenVS() : ShaderStage(GL_VERTEX_SHADER, "PASFullscreenVS") {
+			setFilepath("fullscreen_quad.glsl");
+		}
+	};
+
+	// Precompute transmittance LUT
+	class PASTransmittanceFS : public ShaderStage {
+	public:
+		PASTransmittanceFS() : ShaderStage(GL_FRAGMENT_SHADER, "PASTransmittanceFS") {
+			setFilepath("atmosphere_precompute.glsl");
+			addDefine("PRECOMPUTE_TRANSMITTANCE 1");
+		}
+	};
+
+	DEFINE_SHADER_PROGRAM2(Program_PASTransmittance, PASFullscreenVS, PASTransmittanceFS);
+
+	void init_precomputeTransmittance(OpenGLDevice* device) {
+		// #todo-atmosphere: for test
+		GLuint lut;
+		GLuint fbo;
+		gRenderDevice->createTextures(GL_TEXTURE_2D, 1, &lut);
+		gRenderDevice->objectLabel(GL_TEXTURE, lut, -1, "LUT_Transmittance");
+		gRenderDevice->createFramebuffers(1, &fbo);
+		gRenderDevice->objectLabel(GL_FRAMEBUFFER, fbo, -1, "FBO_LUT_Transmittance");
+
+		PlaneGeometry* fullscreenQuad = new PlaneGeometry(1.0, 1.0);
+
+		ENQUEUE_RENDER_COMMAND([lut, fbo, fullscreenQuad](RenderCommandList& cmdList) {
+			ShaderProgram& program = FIND_SHADER_PROGRAM(Program_PASTransmittance);
+			const int32 WIDTH = 64;
+			const int32 HEIGHT = 256;
+
+			cmdList.textureStorage2D(lut, 1, GL_RGBA32F, WIDTH, HEIGHT);
+
+			cmdList.bindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo);
+			cmdList.viewport(0, 0, WIDTH, HEIGHT);
+			cmdList.useProgram(program.getGLName());
+			cmdList.namedFramebufferTexture(fbo, GL_COLOR_ATTACHMENT0, lut, 0);
+			fullscreenQuad->activate_position_uv(cmdList);
+			fullscreenQuad->activateIndexBuffer(cmdList);
+			fullscreenQuad->drawPrimitive(cmdList);
+		});
+
+		LOG(LogDebug, "Generate transmittance LUT (%s)", "LUT_Transmittance");
+	}
+	void destroy_precomputeTransmittance(OpenGLDevice* device) {
+		//
+	}
+
+	DEFINE_GLOBAL_RENDER_ROUTINE(PAS, init_precomputeTransmittance, destroy_precomputeTransmittance);
+}
 
 namespace pathos {
 
---
+++ shaders/atmosphere_precompute.glsl
@@ -0,0 +1,167 @@
+#version 450 core
+
+#include "deferred_common.glsl"
+
+#ifndef PRECOMPUTE_TRANSMITTANCE
+    #define PRECOMPUTE_TRANSMITTANCE 0
+#endif
+
+in VS_OUT {
+	vec2 screenUV;
+} fs_in;
+
+layout (location = 0) out vec4 outResult;
+
+//////////////////////////////////////////////////////////////////////////
+// Constants
+#define PI                    3.14159265359
+// Unit: meters
+#define EARTH_RADIUS          6.36e6
+#define ATMOSPHERE_RADIUS     6.42e6
+#define MAX_ALTITUDE          (ATMOSPHERE_RADIUS - EARTH_RADIUS)
+#define Hr                    7.994e3
+#define Hm                    1.2e3
+// Unit: 1 / meters
+#define BetaR                 vec3(5.8e-6, 13.5e-6, 33.1e-6)
+#define BetaM                 vec3(21e-6)
+
+//////////////////////////////////////////////////////////////////////////
+// Ray, sphere code from https://www.shadertoy.com/view/XtBXDw
+struct ray_t {
+	vec3 origin;
+	vec3 direction;
+};
+
+struct sphere_t {
+	vec3 origin;
+	float radius;
+};
+
+struct hit_t {
+	float t;
+	vec3 normal;
+	vec3 origin;
+};
+
+#define max_dist 1e8
+hit_t no_hit = hit_t(
+	float(max_dist + 1e1), // 'infinite' distance
+	vec3(0.0), // normal
+	vec3(0.0) // origin
+);
+
+bool validHit(hit_t hit) {
+    return hit.t < max_dist;
+}
+
+void intersect_sphere(
+	in ray_t ray,
+	in sphere_t sphere,
+	inout hit_t hit
+){
+	vec3 rc = sphere.origin - ray.origin;
+	float radius2 = sphere.radius * sphere.radius;
+	float tca = dot(rc, ray.direction);
+//	if (tca < 0.) return;
+
+	float d2 = dot(rc, rc) - tca * tca;
+	if (d2 > radius2)
+		return;
+
+	float thc = sqrt(radius2 - d2);
+	float t0 = tca - thc;
+	float t1 = tca + thc;
+
+	if (t0 < 0.) t0 = t1;
+	if (t0 > hit.t)
+		return;
+
+	vec3 impact = ray.origin + ray.direction * t0;
+
+	hit.t = t0;
+	hit.origin = impact;
+	hit.normal = (impact - sphere.origin) / sphere.radius;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+sphere_t earth = sphere_t(vec3(0.0), EARTH_RADIUS);
+sphere_t atmosphere = sphere_t(vec3(0.0), ATMOSPHERE_RADIUS);
+
+float phaseR(float cosTheta)
+{
+    return 3.0 / (16.0 * PI) * (1.0 + cosTheta * cosTheta);
+}
+float phaseM(float t)
+{
+    const float g = 0.76;
+    float gg = g * g;
+    float tt = t * t;
+    float gt = g * t;
+    
+    float num = 3.0 * (1.0 - gg) * (1.0 + tt);
+	float denom = (8.0 * PI) * (2.0 + gg) * pow(1.0 + gg - 2.0 * gt, 1.5);
+    return num / denom;
+}
+
+vec3 precomputeTransmittance(vec2 uv) {
+    const int NUM_INTEG_STEPS = 128;
+
+    // Due to spherical symmetry we only care about case of +X axis.
+    float mu = PI * uv.y; // View-zenith angle
+    vec3 x = vec3(EARTH_RADIUS + uv.x * MAX_ALTITUDE, 0.0, 0.0); // Earth radius + altitude
+    vec3 v = vec3(cos(mu), sin(mu), 0.0); // View direction
+    
+    hit_t hit_atmosphere = no_hit;
+    hit_t hit_earth = no_hit;
+    ray_t ray = ray_t(x, v);
+    intersect_sphere(ray, atmosphere, hit_atmosphere);
+    intersect_sphere(ray, earth, hit_earth);
+    float rayT = 0.0;
+    if (validHit(hit_atmosphere) && validHit(hit_earth)) {
+        rayT = min(hit_atmosphere.t, hit_earth.t);
+    } else if (validHit(hit_atmosphere)) {
+        rayT = hit_atmosphere.t;
+    } else if (validHit(hit_earth)) {
+        rayT = hit_earth.t;
+    } else {
+        return vec3(1.0);
+    }
+    vec3 x0 = x + v * rayT;
+
+    vec3 T = vec3(0.0); // transmittance
+    vec3 dx = (x0 - x) / float(NUM_INTEG_STEPS);
+    float dx_length = length(dx);
+    bool isGround = false;
+
+    for (int i = 0; i < NUM_INTEG_STEPS; i++) {
+        float height = max(0.0, length(x) - EARTH_RADIUS);
+
+        // Accumulate optical depth (converted to transmittance at end of loop)
+        T += (BetaR * exp(-height / Hr)) * dx_length;
+        T += (BetaM * exp(-height / Hm)) * dx_length;
+
+        x += dx;
+    }
+
+    T = exp(-T); // Optical depth to transmittance
+
+    return min(vec3(1.0), T);
+}
+
+void main() {
+    vec2 uv = fs_in.screenUV;
+    outResult = vec4(0.0, 0.0, 0.0, 0.0);
+
+#if PRECOMPUTE_TRANSMITTANCE
+    outResult = vec4(precomputeTransmittance(uv), 1.0);
+#endif
+
+//#if PRECOMPUTE_REFLECTANCE
+    //#todo
+//#endif
+
+//#if PRECOMPUTE_SCATTERING
+    //#todo
+//#endif
+}
