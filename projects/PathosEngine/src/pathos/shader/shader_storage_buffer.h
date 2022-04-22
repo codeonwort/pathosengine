6849abcbb110b3f926a898e07f7e71513ebc9a4b
codeonwort <codeonwort@gmail.com>
2022-04-22 19:42:40 +0900

    Add ShaderStorageBuffer (SSBO wrapper).
    NOTE: Not tested at all. Don't know if it works. This was written to
    solve the DoF issue in previous commit, but the issue was solved without
    using SSBO.

---
+++ projects/PathosEngine/src/pathos/shader/shader_storage_buffer.h
@@ -0,0 +1,70 @@
+#pragma once
+
+// https://www.khronos.org/opengl/wiki/Shader_Storage_Buffer_Object
+
+#include "pathos/render/render_device.h"
+#include "badger/assertion/assertion.h"
+#include "gl_core.h"
+
+namespace pathos {
+
+	// #todo-shader: For now not used anywhere, not tested at all.
+	class ShaderStorageBuffer {
+
+	public:
+		ShaderStorageBuffer()
+			: ssbo(0)
+			, bufferSize(0) {
+		}
+
+		~ShaderStorageBuffer() {
+			safeDestroy();
+		}
+
+		ShaderStorageBuffer(const ShaderStorageBuffer&) = delete;
+		ShaderStorageBuffer& operator=(const ShaderStorageBuffer&) = delete;
+
+		template<typename T> void init() {
+			//bufferSize = (sizeof(T) + 255) & ~255;
+			bufferSize = sizeof(T);
+			gRenderDevice->createBuffers(1, &ssbo);
+
+			// #todo-shader: Support SSBO flags for CPU-side read/write?
+			const GLbitfield flags = 0;
+
+			if (isInRenderThread()) {
+				RenderCommandList& cmdList = gRenderDevice->getImmediateCommandList();
+				cmdList.namedBufferStorage(this->ssbo, this->bufferSize, (GLvoid*)0, flags);
+			} else {
+				// #todo-renderthread: No issue with order of commands?
+				ENQUEUE_RENDER_COMMAND([this](RenderCommandList& cmdList) {
+					cmdList.namedBufferStorage(this->ssbo, this->bufferSize, (GLvoid*)0, flags);
+				});
+			}
+		}
+
+		// #todo-shader: Support SSBO update by CPU?
+		//void update(RenderCommandList& cmdList, GLuint bindingIndex, void* data) {
+		//	CHECK(isInRenderThread());
+		//	CHECK(ssbo != 0);
+		//	cmdList.namedBufferSubData(ssbo, 0, bufferSize, data);
+		//	cmdList.bindBufferBase(GL_SHADER_STORAGE_BUFFER, bindingIndex, ssbo);
+		//}
+
+		// NOTE: No need to call manually if this instance is deallocated before gRenderDevice shutdown.
+		void safeDestroy() {
+			if (ssbo != 0) {
+				gRenderDevice->deleteBuffers(1, &ssbo);
+				ssbo = 0;
+			}
+		}
+
+		inline GLuint getGLName() const { return ssbo; }
+
+	private:
+		GLuint ssbo;
+		uint32 bufferSize;
+
+	};
+
+}
