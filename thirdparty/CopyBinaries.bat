c2a36abbba1fe5d9d26b3ad4810d8e759480f178
Minseok Lee <codeonwort@gmail.com>
2022-09-12 19:47:33 +0900

    Fix fresh build (#27)
    Make the project build and run on my new laptop.
    
    * Add post-build event: Copy some thirdparty binaries to the output directory.
    * Bump up FreeImage v3.17.0 -> v3.18.0 and use newer binaries.

---
+++ thirdparty/CopyBinaries.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal
+
+set SolutionDir=%1
+set SolutionConfig=%2
+
+echo SolutionDir=%SolutionDir%
+echo Configuration=%SolutionConfig%
+
+if not exist "%SolutionDir%\PathosEngine.sln" (
+	echo First argument should be the solution directory, but couldn't find PathosEngine.sln
+	exit
+)
+
+set ValidConfig=0
+if "%SolutionConfig%" == "Debug" set ValidConfig=1
+if "%SolutionConfig%" == "Release" set ValidConfig=1
+if %ValidConfig%==0 (
+	echo Second argument should be Debug or Release
+	exit
+)
+
+if "%SolutionConfig%" == "Debug" set FreeGLUTFile=freeglutd.dll
+if "%SolutionConfig%" == "Release" set FreeGLUTFile=freeglut.dll
+
+xcopy /y /d "%SolutionDir%\thirdparty\freeglut\binary\%FreeGLUTFile%" "%SolutionDir%\bin\%SolutionConfig%\"
+xcopy /y /d "%SolutionDir%\thirdparty\freeimage-3.18.0\binary\freeimage.dll" "%SolutionDir%\bin\%SolutionConfig%\"
+
+endlocal
