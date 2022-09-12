2404add5a403b7afca3e4d4b4204ae6447708869
codeonwort <codeonwort@gmail.com>
2022-09-12 16:29:12 +0900

    Add post-build event: Copy some thirdparty binaries to bin folder

---
+++ thirdparty/CopyBinaries.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal
+
+set SolutionDir=%1
+set SolutionConfig=%2
+
+echo SolutionDir=%SolutionDir%
+echo Configuration=%SolutionConfig%
+
+if not exist "%SolutionDir%\PathosEngine.sln" (
+	echo First argument should be the solution directory, but couldn't find PathosEngine.sln
+	exit
+)
+
+set ValidConfig=0
+if "%SolutionConfig%" == "Debug" set ValidConfig=1
+if "%SolutionConfig%" == "Release" set ValidConfig=1
+if %ValidConfig%==0 (
+	echo Second argument should be Debug or Release
+	exit
+)
+
+xcopy /y /d "%SolutionDir%\thirdparty\freeglut\binary\freeglut.dll" "%SolutionDir%\bin\%SolutionConfig%\"
+xcopy /y /d "%SolutionDir%\thirdparty\freeimage\binary\freeimage.dll" "%SolutionDir%\bin\%SolutionConfig%\"
+
+endlocal
