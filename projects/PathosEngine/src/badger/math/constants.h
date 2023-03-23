1be5deb269b9bec286cba452227836f471ab934c
codeonwort <codeonwort@gmail.com>
2023-03-23 23:47:23 +0900

    Header for math constants

---
+++ projects/PathosEngine/src/badger/math/constants.h
@@ -0,0 +1,35 @@
+#pragma once
+
+// https://learn.microsoft.com/en-us/cpp/c-runtime-library/math-constants
+
+namespace badger {
+
+	constexpr double d_E           = 2.71828182845904523536;
+	constexpr double d_LOG2E       = 1.44269504088896340736;
+	constexpr double d_LOG10E      = 0.434294481903251827651;
+	constexpr double d_LN2         = 0.693147180559945309417;
+	constexpr double d_LN10        = 2.30258509299404568402;
+	constexpr double d_PI          = 3.14159265358979323846;
+	constexpr double d_PI_2        = 1.57079632679489661923;
+	constexpr double d_PI_4        = 0.785398163397448309616;
+	constexpr double d_1_PI        = 0.318309886183790671538;
+	constexpr double d_2_PI        = 0.636619772367581343076;
+	constexpr double d_2_SQRTPI    = 1.12837916709551257390;
+	constexpr double d_SQRT2       = 1.41421356237309504880;
+	constexpr double d_SQRT1_2     = 0.707106781186547524401;
+
+	constexpr float f_E           = 2.71828182845904523536f;
+	constexpr float f_LOG2E       = 1.44269504088896340736f;
+	constexpr float f_LOG10E      = 0.434294481903251827651f;
+	constexpr float f_LN2         = 0.693147180559945309417f;
+	constexpr float f_LN10        = 2.30258509299404568402f;
+	constexpr float f_PI          = 3.14159265358979323846f;
+	constexpr float f_PI_2        = 1.57079632679489661923f;
+	constexpr float f_PI_4        = 0.785398163397448309616f;
+	constexpr float f_1_PI        = 0.318309886183790671538f;
+	constexpr float f_2_PI        = 0.636619772367581343076f;
+	constexpr float f_2_SQRTPI    = 1.12837916709551257390f;
+	constexpr float f_SQRT2       = 1.41421356237309504880f;
+	constexpr float f_SQRT1_2     = 0.707106781186547524401f;
+
+}
