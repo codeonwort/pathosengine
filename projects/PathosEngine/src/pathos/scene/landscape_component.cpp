b024d5edcdd9af1ea0e02f8f48eaf394b6f27190
codeonwort <codeonwort@gmail.com>
2024-10-23 22:59:34 +0900

    Unify redundant calculateWorldBounds() definitions

--- projects/PathosEngine/src/pathos/scene/landscape_component.cpp
+++ projects/PathosEngine/src/pathos/scene/landscape_component.cpp
@@ -28,25 +28,6 @@ struct LandscapeSectorParameter {
 	float   _pad0;
 };
 
-static AABB calculateWorldBounds(const AABB& localBounds, const matrix4& localToWorld) {
-	vector3 minB = localBounds.minBounds;
-	vector3 maxB = localBounds.maxBounds;
-	vector3 vs[8];
-	for (uint32 i = 0; i < 8; ++i) {
-		vs[i].x = (i & 1) ? minB.x : maxB.x;
-		vs[i].y = ((i >> 1) & 1) ? minB.y : maxB.y;
-		vs[i].z = ((i >> 2) & 1) ? minB.z : maxB.z;
-	}
-	minB = vector3(std::numeric_limits<float>::max());
-	maxB = vector3(std::numeric_limits<float>::lowest());
-	for (uint32 i = 0; i < 8; ++i) {
-		vs[i] = vector3(localToWorld * vector4(vs[i], 1.0f));
-		minB = glm::min(minB, vs[i]);
-		maxB = glm::max(maxB, vs[i]);
-	}
-	return AABB::fromMinMax(minB, maxB);
-}
-
 namespace pathos {
 
 	static const int32 LANDSCAPE_BASE_DIVISIONS = 16;
@@ -270,7 +251,7 @@ namespace pathos {
 					vector3 minV(sizeX * (float)sectorX, sizeY * (float)sectorY, 0.0f);
 					vector3 maxV = minV + vector3(sizeX, sizeY, heightMultiplier);
 					AABB localBounds = AABB::fromMinMax(minV, maxV);
-					AABB worldBounds = calculateWorldBounds(localBounds, getLocalMatrix());
+					AABB worldBounds = badger::calculateWorldBounds(localBounds, getLocalMatrix());
 					bool inFrustum = badger::hitTest::AABB_frustum_noFarPlane(worldBounds, frustum);
 					if (!inFrustum) {
 						continue;
--- projects/PathosEngine/src/pathos/scene/static_mesh_component.cpp
+++ projects/PathosEngine/src/pathos/scene/static_mesh_component.cpp
@@ -1,28 +1,11 @@
 #include "static_mesh_component.h"
 #include "pathos/mesh/mesh.h"
 #include "pathos/render/scene_proxy.h"
+
+#include "badger/math/hit_test.h"
 #include <limits>
 
 namespace pathos {
-
-	static AABB calculateWorldBounds(const AABB& localBounds, const matrix4& localToWorld) {
-		vector3 minB = localBounds.minBounds;
-		vector3 maxB = localBounds.maxBounds;
-		vector3 vs[8];
-		for (uint32 i = 0; i < 8; ++i) {
-			vs[i].x = (i & 1) ? minB.x : maxB.x;
-			vs[i].y = ((i >> 1) & 1) ? minB.y : maxB.y;
-			vs[i].z = ((i >> 2) & 1) ? minB.z : maxB.z;
-		}
-		minB = vector3(std::numeric_limits<float>::max());
-		maxB = vector3(std::numeric_limits<float>::lowest());
-		for (uint32 i = 0; i < 8; ++i) {
-			vs[i] = vector3(localToWorld * vector4(vs[i], 1.0f));
-			minB = glm::min(minB, vs[i]);
-			maxB = glm::max(maxB, vs[i]);
-		}
-		return AABB::fromMinMax(minB, maxB);
-	}
 
 	void StaticMeshComponent::createRenderProxy(SceneProxy* scene) {
 		if (mesh == nullptr || getVisibility() == false) {
@@ -39,7 +22,7 @@ namespace pathos {
 				proxy->modelMatrix = getLocalMatrix();
 				proxy->geometry = geoms[i];
 				proxy->material = materials[i];
-				proxy->worldBounds = calculateWorldBounds(proxy->geometry->getLocalBounds(), proxy->modelMatrix);
+				proxy->worldBounds = badger::calculateWorldBounds(proxy->geometry->getLocalBounds(), proxy->modelMatrix);
 				proxy->doubleSided = mesh->doubleSided;
 				proxy->renderInternal = mesh->renderInternal;
 
@@ -55,7 +38,7 @@ namespace pathos {
 			proxy->prevModelMatrix = prevModelMatrix;
 			proxy->geometry = geoms[i];
 			proxy->material = materials[i];
-			proxy->worldBounds = calculateWorldBounds(proxy->geometry->getLocalBounds(), proxy->modelMatrix);
+			proxy->worldBounds = badger::calculateWorldBounds(proxy->geometry->getLocalBounds(), proxy->modelMatrix);
 
 			scene->addStaticMeshProxy(proxy);
 		}
@@ -68,9 +51,9 @@ namespace pathos {
 		AABB total = AABB::fromMinMax(vector3(0.0f), vector3(0.0f));
 		auto& geoms = mesh->getGeometries();
 		if (geoms.size() > 0) {
-			total = calculateWorldBounds(geoms[0]->getLocalBounds(), getLocalMatrix());
+			total = badger::calculateWorldBounds(geoms[0]->getLocalBounds(), getLocalMatrix());
 			for (size_t i = 1; i < geoms.size(); ++i) {
-				AABB temp = calculateWorldBounds(geoms[i]->getLocalBounds(), getLocalMatrix());
+				AABB temp = badger::calculateWorldBounds(geoms[i]->getLocalBounds(), getLocalMatrix());
 				total = temp + total;
 			}
 		}
@@ -96,7 +79,7 @@ namespace pathos {
 			proxy->modelMatrix = getLocalMatrix();
 			proxy->geometry = G;
 			proxy->material = M;
-			proxy->worldBounds = calculateWorldBounds(G->getLocalBounds(), proxy->modelMatrix);
+			proxy->worldBounds = badger::calculateWorldBounds(G->getLocalBounds(), proxy->modelMatrix);
 
 			outProxyList.push_back(proxy);
 		}
