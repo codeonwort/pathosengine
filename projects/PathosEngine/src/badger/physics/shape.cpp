5d84a330993c70a34f77b3c65f6d93372555f6e3
codeonwort <codeonwort@gmail.com>
2024-11-25 22:49:49 +0900

    Angular velocity

--- projects/PathosEngine/src/badger/physics/shape.cpp
+++ projects/PathosEngine/src/badger/physics/shape.cpp
@@ -1,11 +1,31 @@
 #include "shape.h"
 
+#include "glm/gtx/quaternion.hpp"
+
+// #todo-physics: Hard-coded max angular speed
+static const float MAX_ANGULAR_SPEED = 30.0f;
+
 // p : position, q : orientation
 static vector3 rotatePoint(const vector3& p, const quat& q) {
 	return vector3(glm::mat4_cast(q) * vector4(p, 1.0f));
 }
 
 namespace badger {
+	
+	// ShapeSphere
+	namespace physics {
+
+		matrix3 ShapeSphere::inertiaTensor() const {
+			matrix3 tensor(0.0f);
+			tensor[0][0] = 2.0f * radius * radius / 5.0f;
+			tensor[1][1] = 2.0f * radius * radius / 5.0f;
+			tensor[2][2] = 2.0f * radius * radius / 5.0f;
+			return tensor;
+		}
+
+	} // end of physics (ShapeSphere)
+
+	// Body
 	namespace physics {
 
 		vector3 Body::getCenterOfMassWorldSpace() const {
@@ -31,6 +51,20 @@ namespace badger {
 			return worldSpace;
 		}
 
+		matrix3 Body::getInverseInertiaTensorBodySpace() const {
+			matrix3 tensor = shape->inertiaTensor();
+			matrix3 invTensor = glm::inverse(tensor) * invMass;
+			return invTensor;
+		}
+
+		matrix3 Body::getInverseInertiaTensorWorldSpace() const {
+			matrix3 tensor = shape->inertiaTensor();
+			matrix3 invTensor = glm::inverse(tensor) * invMass;
+			matrix3 orient = glm::toMat3(orientation);
+			invTensor = orient * invTensor * glm::transpose(orient);
+			return invTensor;
+		}
+
 		void Body::applyImpulseLinear(const vector3& impulse) {
 			if (0.0f == invMass) {
 				return;
@@ -42,5 +76,19 @@ namespace badger {
 			linearVelocity += impulse * invMass;
 		}
 
-	}
-}
+		void Body::applyImpulseAngular(const vector3& impulse) {
+			if (invMass == 0.0f) return;
+
+			// L = I w = r x p
+			// dL = I dw = r x J
+			// => dw = I^-1 * ( r x J )
+			angularVelocity += getInverseInertiaTensorWorldSpace() * impulse;
+			if (glm::length2(angularVelocity) > MAX_ANGULAR_SPEED * MAX_ANGULAR_SPEED) {
+				angularVelocity = glm::normalize(angularVelocity);
+				angularVelocity *= MAX_ANGULAR_SPEED;
+			}
+		}
+
+	} // end of physics (Body)
+
+} // end of badger
--- projects/PathosEngine/src/badger/physics/shape.h
+++ projects/PathosEngine/src/badger/physics/shape.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "badger/types/vector_types.h"
+#include "badger/types/matrix_types.h"
 
-#include <glm/gtc/quaternion.hpp>
-#include <glm/fwd.hpp>
+#include "glm/gtc/quaternion.hpp"
+#include "glm/fwd.hpp"
+
 using quat = glm::quat;
 
 namespace badger {
@@ -15,10 +17,11 @@ namespace badger {
 			enum class EShapeType { Sphere };
 
 			virtual EShapeType getType() const = 0;
+			virtual matrix3 inertiaTensor() const = 0;
 			virtual vector3 getCenterOfMass() const { return centerOfMass; }
 
 		protected:
-			vector3 centerOfMass;
+			vector3 centerOfMass = vector3(0.0f);
 
 		};
 
@@ -31,6 +34,7 @@ namespace badger {
 			}
 
 			EShapeType getType() const override { return EShapeType::Sphere; }
+			matrix3 inertiaTensor() const override;
 
 			inline float getRadius() const { return radius; }
 			inline void setRadius(float value) { radius = value; }
@@ -52,6 +56,9 @@ namespace badger {
 			vector3 worldSpaceToBodySpace(const vector3& position) const;
 			vector3 bodySpaceToWorldSpace(const vector3& position) const;
 
+			matrix3 getInverseInertiaTensorBodySpace() const;
+			matrix3 getInverseInertiaTensorWorldSpace() const;
+
 			inline vector3 getPosition() const { return position; }
 			inline void setPosition(const vector3& inPosition) { position = inPosition; }
 
@@ -65,11 +72,13 @@ namespace badger {
 			inline void setLinearVelocity(const vector3& inVeocity) { linearVelocity = inVeocity; }
 
 			void applyImpulseLinear(const vector3& impulse);
+			void applyImpulseAngular(const vector3& impulse);
 
 		private:
 			vector3 position = vector3(0.0f);
 			quat orientation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
 			vector3 linearVelocity = vector3(0.0f);
+			vector3 angularVelocity = vector3(0.0f);
 
 			float invMass = 0.0f;
 			float elasticity = 1.0f;
