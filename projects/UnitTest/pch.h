3cfb8b2c140dc2618d515bdd903c143244b299b1
codeonwort <codeonwort@gmail.com>
2025-01-18 16:49:58 +0900

    Update PCH for unit test project

--- projects/UnitTest/pch.h
+++ projects/UnitTest/pch.h
@@ -1,6 +1,19 @@
 ﻿#ifndef PCH_H
 #define PCH_H
 
-//
+// STL
+#include <vector>
+#include <string>
+#include <map>
+
+// Very long files
+#include "pathos/rhi/render_command_list.h"
+#include "pathos/rhi/render_commands.h"
+
+#include "pathos/smart_pointer.h"
+
+#include "badger/types/int_types.h"
+#include "badger/types/vector_types.h"
+#include "badger/types/matrix_types.h"
 
 #endif //PCH_H
