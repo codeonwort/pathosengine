b03e42c8de4edefda63676ef2145a89f1044a83f
codeonwort <codeonwort@gmail.com>
2025-04-20 17:07:10 +0900

    Optimize diffuse SH shader (step 1).
    SkyAtmosphere screen + cubemap + diffuse SH
    total cost 23 ms -> 7 ms

--- shaders/compute_diffuse_sh.glsl
+++ shaders/compute_diffuse_sh.glsl
@@ -4,19 +4,30 @@
 
 // "An Efficient Representation for Irradiance Environment Maps" by Ravi Ramamoorthi and Pat Hanrahan.
 
-layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
-
-layout (location = 1) uniform int cubemapSize;
-
-layout (binding = 0) uniform samplerCube inCubemap;
+// --------------------------------------------------------
+// Definitions
 
 struct SHBuffer {
     vec4 Ls[9]; // w not used
 };
 
+// --------------------------------------------------------
+// Layout
+
+layout (local_size_x = 1, local_size_y = 1, local_size_z = 6) in;
+
+layout (location = 1) uniform int cubemapSize;
+
+layout (binding = 0) uniform samplerCube inCubemap;
+
 layout (std140, binding = 2) writeonly buffer Buffer_SH {
 	SHBuffer outSH;
 };
+
+shared vec3 s_faceL[6][9];
+
+// --------------------------------------------------------
+// Shader
 
 float Y00  (vec3 dir) { return 0.282095f; };
 float Y1_1 (vec3 dir) { return 0.488603f * dir.y; };
@@ -28,7 +39,7 @@ float Y20  (vec3 dir) { return 0.315392f * (3.0f * dir.z * dir.z - 1.0f); };
 float Y21  (vec3 dir) { return 1.092548f * dir.x * dir.z; };
 float Y22  (vec3 dir) { return 0.546274f * (dir.x * dir.x - dir.y * dir.y); };
 
-vec3 getDir(int face, float u, float v) {
+vec3 getDir(uint face, float u, float v) {
     if (face == 0) return normalize(vec3(1, u, v));
     else if (face == 1) return normalize(vec3(-1, u, v));
     else if (face == 2) return normalize(vec3(u, 1, v));
@@ -38,40 +49,38 @@ vec3 getDir(int face, float u, float v) {
     return vec3(0);
 }
 
-void prefilter() {
+void prefilter(uint face) {
     vec3 scratch[9];
     for (int i = 0; i < 9; i++) scratch[i] = vec3(0);
     float wSum = 0.0;
 
-    for (int face = 0; face < 6; face++) {
-        for (int y = 0; y < cubemapSize; y++) {
-            for (int x = 0; x < cubemapSize; x++) {
-                float u = 2.0 * (float(x) / float(cubemapSize)) - 1.0;
-                float v = 2.0 * (float(y) / float(cubemapSize)) - 1.0;
-                float tmp = 1.0 + u * u + v * v;
-                float w = 4.0 / (tmp * sqrt(tmp));
+    for (int y = 0; y < cubemapSize; y++) {
+        for (int x = 0; x < cubemapSize; x++) {
+            float u = 2.0 * (float(x) / float(cubemapSize)) - 1.0;
+            float v = 2.0 * (float(y) / float(cubemapSize)) - 1.0;
+            float tmp = 1.0 + u * u + v * v;
+            float w = 4.0 / (tmp * sqrt(tmp));
 
-                vec3 dir = getDir(face, u, v);
-                vec3 sky = w * textureLod(inCubemap, dir, 0).xyz;
+            vec3 dir = getDir(face, u, v);
+            vec3 sky = w * textureLod(inCubemap, dir, 0).xyz;
 
-                scratch[0] += sky * Y00(dir);
-                scratch[1] += sky * Y1_1(dir);
-                scratch[2] += sky * Y10(dir);
-                scratch[3] += sky * Y11(dir);
-                scratch[4] += sky * Y2_2(dir);
-                scratch[5] += sky * Y2_1(dir);
-                scratch[6] += sky * Y20(dir);
-                scratch[7] += sky * Y21(dir);
-                scratch[8] += sky * Y22(dir);
+            scratch[0] += sky * Y00(dir);
+            scratch[1] += sky * Y1_1(dir);
+            scratch[2] += sky * Y10(dir);
+            scratch[3] += sky * Y11(dir);
+            scratch[4] += sky * Y2_2(dir);
+            scratch[5] += sky * Y2_1(dir);
+            scratch[6] += sky * Y20(dir);
+            scratch[7] += sky * Y21(dir);
+            scratch[8] += sky * Y22(dir);
 
-                wSum += w;
-            }
+            wSum += w;
         }
     }
 
     float norm = 4.0 * PI / wSum;
     for (int i = 0; i < 9; i++) {
-        outSH.Ls[i] = vec4(scratch[i] * norm, 0);
+        s_faceL[face][i] = scratch[i] * norm;
     }
 }
 
@@ -104,5 +113,27 @@ vec3 evaluateSH(SHBuffer shBuffer, vec3 dir) {
 }
 
 void main() {
-    prefilter();
+    const uint face = gl_LocalInvocationID.z;
+
+    for (int i = 0; i < 9; i++) {
+        s_faceL[face][i] = vec3(0);
+    }
+    memoryBarrierShared();
+
+    prefilter(face);
+    memoryBarrierShared();
+
+    if (gl_LocalInvocationID.x == 0) {
+        for (int i = 0; i < 9; i++) {
+            vec3 L = vec3(0);
+            L += s_faceL[0][i];
+            L += s_faceL[1][i];
+            L += s_faceL[2][i];
+            L += s_faceL[3][i];
+            L += s_faceL[4][i];
+            L += s_faceL[5][i];
+
+            outSH.Ls[i] = vec4(L, 0);
+        }
+    }
 }
